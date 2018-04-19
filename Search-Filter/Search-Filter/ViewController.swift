441395654f8730cae066cbc1d98d67ed7b2f92ce
antonio yaphiar <antonio.y@kiranatama.com>
2018-04-20 03:18:40 +0700

    project init

---
+++ Search-Filter/Search-Filter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Search-Filter
+//
+//  Created by antonio yaphiar on 4/20/18.
+//  Copyright © 2018 Antonio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
